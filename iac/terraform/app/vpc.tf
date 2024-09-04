804dafbebed9d50e0c1c6b9553ef1c9e0d9a8c37
David Vega <dukeofgaming@gmail.com>
2024-09-04 15:39:00 -0700

    Added SG rule to allow connection to ECR

--- iac/terraform/app/vpc.tf
+++ iac/terraform/app/vpc.tf
@@ -24,3 +24,11 @@ module "vpc" {
     "10.0.102.0/24"
   ]
 }
+resource "aws_security_group_rule" "allow_outbound_https" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpc.default_security_group_id
+}
