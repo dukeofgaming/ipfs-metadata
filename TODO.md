6b15570499c1099da5ccaf5930dc3de952f6e711
David Vega <dukeofgaming@gmail.com>
2024-09-04 18:17:35 -0700

    Cleaning up, updating docs and to-do

--- TODO.md
+++ TODO.md
@@ -6,14 +6,17 @@ NFT IPFS metadata fetcher.
 
 ### Todo
 
-- [ ] Setup CI/CD pipeline  
+- [ ] Create Github Actions for image build and push
+- [ ] Create Github Actions for terraform deployment
 
 ### In Progress
 
-- [ ] Create IaC with Terraform for AWS infrastructure  
+- [ ] Create Terraform for RDS
 
 ### Done ✓
 
+- [x] Create Terraform for ECS
+- [x] Create Terraform environment for AWS
 - [x] Setup local development environment with Postgres  
 - [x] Containerize application  
 
--- iac/terraform/app/ecs-iam.tf
+++ iac/terraform/app/ecs-iam.tf
@@ -22,29 +22,3 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
-#Public ECR Policy
-# data "aws_iam_policy_document" "ecr_policy_document" {
-#   version = "2012-10-17"
-
-#   statement {
-#     actions = [
-#       "ecr-public:GetDownloadUrlForLayer",
-#       "ecr-public:BatchGetImage",
-#       "ecr-public:BatchCheckLayerAvailability"
-#     ]
-#     effect    = "Allow"
-#     resources = ["*"]
-#   }
-# }
-
-# resource "aws_iam_policy" "ecr_policy" {
-#   name        = "ecr_policy"
-#   description = "Allow ECS to pull images from ECR"
-#   policy      = data.aws_iam_policy_document.ecr_policy_document.json
-# }
-
-# resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
-#   role       = aws_iam_role.ecs_task_execution_role.name
-#   policy_arn = aws_iam_policy.ecr_policy.arn
-# }
