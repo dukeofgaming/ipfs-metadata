2f144fc790e5e8046c2b235ba59c9ed47b815141
David Vega <dukeofgaming@gmail.com>
2024-09-09 16:22:06 -0700

    Generating branch-environment-map for Github Actions

--- iac/terraform/core/main.tf
+++ iac/terraform/core/main.tf
@@ -3,4 +3,29 @@ locals {
     for pipeline in var.pipelines :
     pipeline.environment => pipeline.aws_iam_user.name == null ? true : false
   })
+
+  branch_environment_map = {
+    for pipeline in values(var.pipelines): 
+      pipeline.branch => pipeline.environment
+  }
+}
+
+# Create branch-environment-map.json with branches/patterns as keys
+# and environments as values
+
+resource "null_resource" "branch_environment_map" {
+  
+  triggers = {
+    branches_changed      = jsonencode(keys(local.branch_environment_map))
+    environments_changed  = jsonencode(values(local.branch_environment_map))
+    #always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      echo '${jsonencode(local.branch_environment_map)}' \
+        > branch-environment-map.json
+    EOF
+  }
+  
 }
