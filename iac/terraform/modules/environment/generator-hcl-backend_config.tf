74df6f8593c9877506f0f931f7aa5665bbfbc69b
David Vega <dukeofgaming@gmail.com>
2024-09-11 03:05:40 -0700

    Refactoring to local file provisioner

--- iac/terraform/modules/environment/generator-hcl-backend_config.tf
+++ iac/terraform/modules/environment/generator-hcl-backend_config.tf
@@ -6,21 +6,14 @@ locals {
     backend_lock_table_id   = module.s3_backend.lock_table.id
 }
 
-resource "null_resource" "update_backend_hcl" {
+resource "local_file" "backend_hcl" {
   count = var.generated_hcl_file_path != null ? 1 : 0
 
-  triggers = {
-    s3_bucket_update = local.backend_bucket_id
-    s3_table_update  = local.backend_lock_table_id
-    always_run = "${timestamp()}"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo 'bucket          = "${local.backend_bucket_id}"' > ${var.generated_hcl_file_path}
-      echo 'dynamodb_table  = "${local.backend_lock_table_id}"' >> ${var.generated_hcl_file_path}
-      echo 'key             = "${var.key}"' >> ${var.generated_hcl_file_path}
-      echo 'region          = "${data.aws_region.current.name}"' >> ${var.generated_hcl_file_path}
-    EOT
-  }
+  filename = "${var.generated_hcl_file_path}"
+  content = <<-EOF
+    bucket          = "${local.backend_bucket_id}"
+    dynamodb_table  = "${local.backend_lock_table_id}"
+    key             = "${var.key}"
+    region          = "${data.aws_region.current.name}"
+  EOF
 }
