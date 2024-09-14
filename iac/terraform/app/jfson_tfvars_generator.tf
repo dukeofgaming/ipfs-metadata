d0241415b7bff3a8735f1113c3b66689de3ca027
David Vega <dukeofgaming@gmail.com>
2024-09-13 17:50:52 -0700

    Seeing what committing environment tfvars could look like

--- iac/terraform/app/jfson_tfvars_generator.tf
+++ iac/terraform/app/jfson_tfvars_generator.tf
@@ -2,9 +2,9 @@
 # The creation of this file will inform developers of what applied and where
 # but the file will not be automatically consumed by the pipeline 
 # if put in version control.
-resource "local_file" "foo" {
+resource "local_file" "environment_tfvars" {
   content  = jsonencode({
-    # Passed by the user or pipeline on a first run
+    # Passed by the pipeline by -var or user from local
     environment         = var.environment           
     container_image     = "${var.container_image}"
 
